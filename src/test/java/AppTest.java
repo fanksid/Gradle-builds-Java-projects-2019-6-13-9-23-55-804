824f314e75ee7694b73b9f3d074dc869fc2b2211
Kong Fanyue <fykong@thoughtworks.com>
2019-06-28 15:10:02 +0800

    [Task]-Add java src and build.gradle

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AppTest {
+
+    @Test
+    public void testMockClass() {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        String value = "first";
+        when(mockedList.get(0)).thenReturn(value);
+
+        assertEquals(mockedList.get(0), value);
+
+    }
+
+}
